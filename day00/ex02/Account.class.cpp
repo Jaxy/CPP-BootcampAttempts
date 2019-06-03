c6731df45244c04363b4d731c313b5b4d582dbff
jasmine DE-AGRELA <jde-agr@c2r5s6.wethinkcode.co.za>
2019-06-03 16:38:43 +0200

    partial ex02 solution

---
+++ day00/ex02/Account.class.cpp
@@ -0,0 +1,37 @@
+#include "Account.class.hpp"
+
+Account::Account() {
+    return;
+}
+
+Account::Account(int initial_deposit) {
+    makeDeposit(initial_deposit);
+}
+
+Account::~Account() {
+    return;
+}
+
+void Account::makeDeposit(int deposit) {
+    this->_amount = this->_amount + deposit;
+}
+
+bool Account::makeWithdrawal(int withdrawal) {
+    if (withdrawal > this->_amount) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+int Account::checkAmount(void) const {
+    return this->_amount;
+}
+
+void Account::displayStatus(void) const {
+
+}
+
+void Account::_displayTimestamp(void) {
+
+}
