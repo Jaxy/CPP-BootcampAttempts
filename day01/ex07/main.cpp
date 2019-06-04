6dcfd339bdaff7a60d7143f75d929fea413d6de4
jde-agr <jde-agr@student.wethinkcode.co.za>
2019-06-04 21:54:58 +0200

    Updated ex07 with replace logic, inclusion of makefile and test

--- day01/ex07/main.cpp
+++ day01/ex07/main.cpp
@@ -1,51 +1,45 @@
 #include <iostream>
 #include <fstream>
-// #include <string>
-
-int findWord(std::string search, std::string replace) {
-    // std::string str = "This foo    isafoostringfoo.";
-    int indx = 0;
-    int found = 0;// = str.find("foo");
-    std::string dub = search.substr(found, search.length());
-    // std::cout << "Now: " << found << "Prev : " << prev << std::endl;
-    while (indx < (int)search.length()) {
-        indx += found;
-        found = dub.find(replace);
-        dub = search.substr(indx);
-        std::string toComp = dub.substr(0, replace.length());
-        if (found && !toComp.compare(replace)) {
-            std::cout << "String: " << dub << "\tFound" << found  << "\tNow: " << indx << std::endl;
-        }
-    }
-    return 0;
-}
+#include <cstring>
 
 int main(int argc, char **argv) {
     if (argc == 4) {
-        int s1Len = strlen(argv[2]);
-        if (s1Len && strlen(argv[3])) {
-            std::ifstream file(argv[1]);
-            // if (file.is_open()) {
-            //     std::string word;
-            //     // int totChar = 0;
-            //     bool first = true;
-            //     while (!file.eof()) {
-            //         getline(file, word, ' ');
-            //         // Search for matching string
-                    
-            //         // End search for matching string
-            //         if (first) {
-            //             first = false;
-            //             std::cout << word << std::flush;
-            //         } else {
-            //             std::cout << " " << word << std::flush;
-            //         }
-            //     }
-            //     file.close();
-            // } else {
-            //     std::cout << "File does not exist" << std::endl;
-            // }
-            findWord("This foo    isafoostringfoo.", "is");
+        std::string s1 = argv[2];
+        std::string s2 = argv[3];
+        int s1Len = s1.length();
+        if (s1Len && s2.length()) {
+            if (s1.compare(s2) != 0) {
+                std::ifstream file(argv[1]);
+                if (file.is_open()) {
+                    std::string newName = (std::string)argv[1] + ".replace";
+                    std::ofstream newFile(newName.c_str());
+                    std::string word;
+                    bool first = true;
+                    while (!file.eof()) {
+                        getline(file, word, ' ');
+                        // Search for matching string
+                        std::size_t findword = word.find(s1);
+                        while (findword != std::string::npos)
+                        {
+                            word.replace(findword, s1Len, s2);
+                            findword = word.find(s1);
+                        }
+                        // End search for matching string
+                        if (first) {
+                            first = false;
+                            newFile << word << std::flush;
+                        } else {
+                            newFile << " " << word << std::flush;
+                        }
+                    }
+                    file.close();
+                    newFile.close();
+                } else {
+                    std::cout << "File does not exist" << std::endl;
+                }
+            } else {
+                std::cout << "s1 and s2 are the same. It's pointless replacing s1 with s2." << std::endl;
+            }
         } else {
             std::cout << "s1 and s2 cannot be EMPTY" << std::endl;
         }
